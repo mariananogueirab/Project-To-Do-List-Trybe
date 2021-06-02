faaca3007091bf02592b0b5f9677f6051273a2fe
mariananogueirab <mariananbp@gmail.com>
2021-06-02 20:33:02 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const title = document.createElement('h1');
 title.innerText = 'Minha Lista de Tarefas';
 title.className = 'titleClass';
 header.appendChild(title);
+
+// Exercício 2:
+
+const marker = document.createElement('p');
+marker.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+marker.id = 'funcionamento';
+header.appendChild(marker);
