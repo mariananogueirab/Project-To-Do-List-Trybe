78bcb823b5d3a457e04c44c5148be723c7912f6c
mariananogueirab <mariananbp@gmail.com>
2021-06-02 20:40:10 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ const marker = document.createElement('p');
 marker.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 marker.id = 'funcionamento';
 header.appendChild(marker);
+
+// Exercício 3:
+
+const divInput = document.createElement('div');
+divInput.className = 'divInputClass';
+main.appendChild(divInput);
+
+const inputTask = document.createElement('input');
+inputTask.type = 'text';
+inputTask.id = 'texto-tarefa';
+divInput.appendChild(inputTask);
